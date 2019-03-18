2d4b36c8ded32701b6a45494bf2ae400a5167f3c
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2019-03-19 01:12:13 +0900

    tcp

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -8,17 +8,9 @@ import (
 func Listen() {
   addr := "localhost:9876"
   fmt.Println(addr)
-	conn, err := net.ListenPacket("udp", addr)
+	conn, err := net.Listen("tcp", addr)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-
-	buffer := make([]byte, 1500)
-
-	for {
-		length, remoteAddr, _ := conn.ReadFrom(buffer)
-		fmt.Printf("Received from %v: %v\n", remoteAddr, string(buffer[:length]))
-		conn.WriteTo([]byte("hello"), remoteAddr)
-	}
 }
