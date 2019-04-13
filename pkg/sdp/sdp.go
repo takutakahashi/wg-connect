476c30e9fa84b96e5569bfc0179ddf3b2bb94338
Takuya TAKAHASHI <takahashi@bank.co.jp>
2019-04-13 14:50:04 +0900

    mod code

--- pkg/sdp/sdp.go
+++ pkg/sdp/sdp.go
@@ -1,116 +1,40 @@
 package sdp
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"net/http"
-	"time"
-
-	"github.com/pions/webrtc"
-	"github.com/pions/webrtc/examples/util"
-	"github.com/pions/webrtc/pkg/datachannel"
-	"github.com/pions/webrtc/pkg/ice"
+	"net"
 )
 
-func StartServer() {
-	addr := flag.String("address", ":50000", "Address to host the HTTP server on.")
-	flag.Parse()
-
-	// Everything below is the pion-WebRTC API! Thanks for using it ❤️.
-
-	// Prepare the configuration
-	config := webrtc.RTCConfiguration{
-		IceServers: []webrtc.RTCIceServer{
-			{
-				URLs: []string{"stun:stun.l.google.com:19302"},
-			},
-		},
+func echo(conn *net.TCPConn) {
+	defer conn.Close()
+	fmt.Println(conn.LocalAddr())
+	for {
+		buf := make([]byte, 1024*4)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(buf[:n]))
 	}
-
-	// Create a new RTCPeerConnection
-	peerConnection, err := webrtc.New(config)
-	util.Check(err)
-
-	// Set the handler for ICE connection state
-	// This will notify you when the peer has connected/disconnected
-	peerConnection.OnICEConnectionStateChange(func(connectionState ice.ConnectionState) {
-		fmt.Printf("ICE Connection State has changed: %s\n", connectionState.String())
-	})
-
-	// Register data channel creation handling
-	peerConnection.OnDataChannel(func(d *webrtc.RTCDataChannel) {
-		fmt.Printf("New DataChannel %s %d\n", d.Label, d.ID)
-
-		// Register channel opening handling
-		d.OnOpen(func() {
-			fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels every 5 seconds\n", d.Label, d.ID)
-
-			for range time.NewTicker(5 * time.Second).C {
-				message := util.RandSeq(15)
-				fmt.Printf("Sending %s \n", message)
-
-				err := d.Send(datachannel.PayloadString{Data: []byte(message)})
-				util.Check(err)
-			}
-		})
-
-		// Register message handling
-		d.OnMessage(func(payload datachannel.Payload) {
-			switch p := payload.(type) {
-			case *datachannel.PayloadString:
-				fmt.Printf("Message '%s' from DataChannel '%s' payload '%s'\n", p.PayloadType().String(), d.Label, string(p.Data))
-			case *datachannel.PayloadBinary:
-				fmt.Printf("Message '%s' from DataChannel '%s' payload '% 02x'\n", p.PayloadType().String(), d.Label, p.Data)
-			default:
-				fmt.Printf("Message '%s' from DataChannel '%s' no payload \n", p.PayloadType().String(), d.Label)
-			}
-		})
-	})
-
-	// Exchange the offer/answer via HTTP
-	offerChan, answerChan := mustSignalViaHTTP(*addr)
-
-	// Wait for the remote SessionDescription
-	offer := <-offerChan
-
-	fmt.Println("------offer---------")
-	fmt.Println(offer)
-	err = peerConnection.SetRemoteDescription(offer)
-	util.Check(err)
-
-	// Sets the LocalDescription, and starts our UDP listeners
-	answer, err := peerConnection.CreateAnswer(nil)
-	util.Check(err)
-
-	// Send the answer
-	answerChan <- answer
-
-	// Block forever
-	select {}
 }
 
-// mustSignalViaHTTP exchange the SDP offer and answer using an HTTP server.
-func mustSignalViaHTTP(address string) (offerOut chan webrtc.RTCSessionDescription, answerIn chan webrtc.RTCSessionDescription) {
-	offerOut = make(chan webrtc.RTCSessionDescription)
-	answerIn = make(chan webrtc.RTCSessionDescription)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var offer webrtc.RTCSessionDescription
-		err := json.NewDecoder(r.Body).Decode(&offer)
-		util.Check(err)
-
-		offerOut <- offer
-		answer := <-answerIn
-		fmt.Println("------answer---------")
-		fmt.Println(answer)
-		err = json.NewEncoder(w).Encode(answer)
-		util.Check(err)
-
-	})
-
-	go http.ListenAndServe(address, nil)
-	fmt.Println("Listening on", address)
-
-	return
+func StartServer() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":50000")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(tcpAddr)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			panic(err)
+		}
+		go echo(conn)
+	}
 }
