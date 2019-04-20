82ee3eed20dc4c6e260f61aade914e4c06542cca
Takuya TAKAHASHI <takahashi@bank.co.jp>
2019-04-20 18:54:49 +0900

    exchange offer

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,29 +2,15 @@ package client
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"github.com/pions/webrtc"
 	"github.com/pions/webrtc/examples/util"
-	"github.com/pions/webrtc/pkg/datachannel"
-	"github.com/pions/webrtc/pkg/ice"
 	pb "github.com/takutakahashi/wg-connect/pkg/proto/sdp_exchange"
 	"google.golang.org/grpc"
 	"time"
 )
 
-type WgSessionDescription struct {
-	Offer webrtc.RTCSessionDescription
-	Token string
-}
-
 func ConnectServer() {
-	addr := flag.String("address", ":50000", "Address that the HTTP server is hosted on.")
-	flag.Parse()
-
-	// Everything below is the pion-WebRTC API! Thanks for using it ❤️.
-
-	// Prepare the configuration
 	config := webrtc.RTCConfiguration{
 		IceServers: []webrtc.RTCIceServer{
 			{
@@ -32,71 +18,31 @@ func ConnectServer() {
 			},
 		},
 	}
-
-	// Create a new RTCPeerConnection
 	peerConnection, err := webrtc.New(config)
 	util.Check(err)
-
-	// Create a datachannel with label 'data'
-	dataChannel, err := peerConnection.CreateDataChannel("data", nil)
-	util.Check(err)
-
-	// Set the handler for ICE connection state
-	// This will notify you when the peer has connected/disconnected
-	peerConnection.OnICEConnectionStateChange(func(connectionState ice.ConnectionState) {
-		fmt.Printf("ICE Connection State has changed: %s\n", connectionState.String())
-	})
-
-	// Register channel opening handling
-	dataChannel.OnOpen(func() {
-		fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels every 5 seconds\n", dataChannel.Label, dataChannel.ID)
-
-		for range time.NewTicker(5 * time.Second).C {
-			message := util.RandSeq(15)
-			fmt.Printf("Sending %s \n", message)
-
-			err := dataChannel.Send(datachannel.PayloadString{Data: []byte(message)})
-			util.Check(err)
-		}
-	})
-
-	// Register the OnMessage to handle incoming messages
-	dataChannel.OnMessage(func(payload datachannel.Payload) {
-		switch p := payload.(type) {
-		case *datachannel.PayloadString:
-			fmt.Printf("Message '%s' from DataChannel '%s' payload '%s'\n", p.PayloadType().String(), dataChannel.Label, string(p.Data))
-		case *datachannel.PayloadBinary:
-			fmt.Printf("Message '%s' from DataChannel '%s' payload '% 02x'\n", p.PayloadType().String(), dataChannel.Label, p.Data)
-		default:
-			fmt.Printf("Message '%s' from DataChannel '%s' no payload \n", p.PayloadType().String(), dataChannel.Label)
-		}
-	})
-
-	// Create an offer to send to the browser
-	offer, err := peerConnection.CreateOffer(nil)
-	util.Check(err)
-
-	// Exchange the offer for the answer
-	mustSignalViaHTTP(offer, *addr)
-
-	// Block forever
-	select {}
-}
-
-// mustSignalViaHTTP exchange the SDP offer and answer using an HTTP Post request.
-func mustSignalViaHTTP(offer webrtc.RTCSessionDescription, address string) webrtc.RTCSessionDescription {
+	token := &pb.Token{Body: "takutaka"}
 	conn, err := grpc.Dial("localhost:50000", grpc.WithInsecure())
-	fmt.Println(conn)
 	util.Check(err)
 	defer conn.Close()
 	c := pb.NewExchangeClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	fmt.Println(c)
-	r, err := c.GetPeer(ctx, &pb.PeerMessage{Token: "aaa"})
-	fmt.Println(r)
-	util.Check(err)
-	fmt.Println(r.BodyJson)
-	var answer webrtc.RTCSessionDescription
-	return answer
+	o, err := c.GetOffer(ctx, token)
+	if err != nil {
+		// offer がない
+		fmt.Println(err)
+		r, err := c.GetPeer(ctx, &pb.PeerMessage{Token: "takutaka"})
+		offer, err := peerConnection.CreateOffer(nil)
+		c.SendOffer(ctx, &pb.OfferMessage{Token: token, Body: offer.Sdp})
+		util.Check(err)
+		fmt.Println(r)
+	} else {
+		// offer があった
+		offer := webrtc.RTCSessionDescription{
+			Type: webrtc.RTCSdpTypeOffer,
+			Sdp:  o.Body,
+		}
+		fmt.Println(offer)
+		fmt.Println("offer found")
+	}
 }
